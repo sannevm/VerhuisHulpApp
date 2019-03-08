e68908a6f234083e3a61b6bf593ffc8022050ceb
Sanne <sannevanmourik@hotmail.com>
2019-03-08 13:07:08 +0100

    Add todo-list-item component and add it to the routing module

--- Angular/ClientApp/src/app/app-routing.module.ts
+++ Angular/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo-list-item', component: TodoListItemComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
